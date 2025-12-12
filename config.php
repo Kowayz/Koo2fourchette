1372bc7d096904a935bde893d1d7f252174029ed
Kowayz <Kowiz91330@gmail.com>
2025-12-12 01:16:49 +0100

    correction bug

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $host = 'localhost';
 $dbname = 'koo_2_fourchette';
 $username = 'ethan';
-$password = 'TON_MOT_DE_PASSE'; // Mets le vrai mot de passe que tu as défini
+$password = 'ethan'; // Mets le vrai mot de passe que tu as défini
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
