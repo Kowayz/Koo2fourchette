bec1ec0f010e9ccd8a3e9d4523a3a0c1610def1e
Kowayz <Kowiz91330@gmail.com>
2026-05-06 14:57:55 +0200

    chore: update MySQL connection charset from UTF8 to latin1

--- config.php
+++ config.php
@@ -8,8 +8,7 @@ $password = 'ethan'; // Utilisez votre vrai mot de passe MySQL ici
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     
-    // CORRECTION ACCENTS : Forcer MySQL à communiquer en UTF8
-    $pdo->exec("SET NAMES 'UTF8'"); 
+    $pdo->exec("SET NAMES 'latin1'");
     
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
